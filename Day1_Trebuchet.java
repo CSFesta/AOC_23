f2440d0b5ed2d4d6f0003b23e1446f40eca3768e
CSFesta <cauafesta@hotmail.com>
2023-12-02 16:53:35 -0300

    Day 1 - Trebuchet - Part2

--- Day1_Trebuchet.java
+++ Day1_Trebuchet.java
@@ -16,27 +16,61 @@ public class Day1_Trebuchet {
 			}
 			scanner.close();
 		} catch (FileNotFoundException e) {
-			System.err.println("Arquivo n�o encontradoas");
+			System.err.println("Arquivo nao encontrado");
 			e.printStackTrace();
 		}
 
 		int total = 0;
 		for (int i = 0; i < list.size(); i++) {
-			String str = "" + "";
+			String str = "";
+			String volta = arrumaNovo(list.get(i));
+			list.set(i, arruma(list.get(i)));
 			for (int j = 0; j < list.get(i).length(); j++) {
 				if (Character.isDigit(list.get(i).charAt(j))) {
 					str += list.get(i).charAt(j);
 					break;
 				}
 			}
-			for (int j = list.get(i).length() - 1; j >= 0; j--) {
-				if (Character.isDigit(list.get(i).charAt(j))) {
-					str += list.get(i).charAt(j);
+			for (int j = volta.length() - 1; j >= 0; j--) {
+				if (Character.isDigit(volta.charAt(j))) {
+					str += volta.charAt(j);
 					break;
 				}
 			}
 			total += Integer.parseInt(str);
 		}
 		System.out.println(total);
+	}
+
+	static String arruma(String str) {
+		str = str.replace("eightwo", "8");
+		str = str.replace("oneight", "1");
+		str = str.replace("twone", "2");
+		str = str.replace("nine", "9");
+		str = str.replace("five", "5");
+		str = str.replace("one", "1");
+		str = str.replace("eight", "8");
+		str = str.replace("two", "2");
+		str = str.replace("three", "3");
+		str = str.replace("four", "4");
+		str = str.replace("six", "6");
+		str = str.replace("seven", "7");
+		return str;
+	}
+
+	static String arrumaNovo(String str) {
+		str = str.replace("eightwo", "2");
+		str = str.replace("twone", "1");
+		str = str.replace("oneight", "8");
+		str = str.replace("nine", "9");
+		str = str.replace("five", "5");
+		str = str.replace("one", "1");
+		str = str.replace("eight", "8");
+		str = str.replace("two", "2");
+		str = str.replace("three", "3");
+		str = str.replace("four", "4");
+		str = str.replace("six", "6");
+		str = str.replace("seven", "7");
+		return str;
 	}
 }
